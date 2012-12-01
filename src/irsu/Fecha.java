6b862b7837e85436cb4c7d9ccb7a5581b7a72c19
gir241 <marco.molina.hidalgo@gmail.com>
2012-12-01 16:19:52 -0300

    solucionado problema con la fecha

--- src/irsu/Fecha.java
+++ src/irsu/Fecha.java
@@ -3,9 +3,10 @@
  * and open the template in the editor.
  */
 package irsu;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-
+import java.util.Date;
 
 /**
  *
@@ -14,17 +15,10 @@ import java.util.GregorianCalendar;
 public class Fecha {
     
     public  static String fechaActual(){
-    
-        
-        
-        
-      GregorianCalendar cal = new GregorianCalendar();
-      String fecha=Integer.toString(cal.get(Calendar.DATE))+"/"+Integer.toString(cal.get(Calendar.MONTH))+"/"+Integer.toString(cal.get(Calendar.YEAR));
-      return fecha;
-   //Calendar cal=Calendar.getInstance();
-       // String fecha=(cal.get(cal.DATE)+"/"+cal.get(cal.MONTH)+"/"+cal.get(cal.YEAR)); 
-        //return fecha;
-        
+ 
+      Calendar c1 = GregorianCalendar.getInstance();
+      SimpleDateFormat fechas = new SimpleDateFormat("dd/MM/yyyy");  
+      String fecha= (fechas.format(c1.getTime()));
+      return fecha;   
     }
-    
 }
